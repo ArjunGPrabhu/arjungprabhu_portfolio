d9a82661784ecb0c9134d3331670e18658a28d3f
Arjun G Prabhu <137024831+ArjunGPrabhu@users.noreply.github.com>
2024-06-14 12:08:55 +0530

    arjungprabhu

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Arjun G Prabhu </span> An MCA (Master of Computer Applications) graduate with experience in Full-Stack (Front-End, Back-End, App-Development), I possess a versatile and comprehensive skill set. This background allows me to approach software development from multiple angles, ensuring well-rounded and robust solutions.<br> I have the ability to create dynamic and responsive web interfaces, manage and optimize server-side processes, and develop engaging mobile applications.<br>"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
