4e6eeda1a03388ef454fc5c9c121dee46b54ae8e
Arjun G Prabhu <137024831+ArjunGPrabhu@users.noreply.github.com>
2024-11-14 15:37:32 +0530

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Arjun G Prabhu", "App Developer", "Web Developer", "Backend Developer"];
+const textArray = ["Arjun G Prabhu", "App", "Web", "Backend"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
